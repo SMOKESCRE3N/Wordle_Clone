5a9ee3cf267299090cae5b75022ae99a68b11b39
SMOKESCRE3N <dixitkartik1203@gmail.com>
2025-03-19 19:14:28 +0530

    some test changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  //base: "Wordle_Clone",
+  base: "/Wordle_Clone/",
 });
